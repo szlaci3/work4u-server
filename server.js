1227aa557a461fe7cb8776956db6dba019a7bec8
Laszlo Szalai <l.szalai@super-staffing.com>
2023-07-03 22:00:25 +0800

    Feat: login module with dummy data;

--- server.js
+++ server.js
@@ -20,56 +20,31 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Retrieve all cards
-app.get('/cards', async (req, res) => {
-  try {
-    // Retrieve the list of card IDs from Redis
-    const cardIds = await redis.lrange('cards', 0, -1);
+// client.set('usernames', JSON.stringify(['22']));
+client.set('passwords', JSON.stringify({'22': '22'}));
 
-    // Retrieve the card objects from Redis using the IDs
-    const cards = await Promise.all(
-      cardIds.map(async (cardId) => {
-        const card = await redis.hgetall(`card:${cardId}`);
-        return card;
-      })
-    );
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
 
-    res.json(cards);
-  } catch (error) {
-    console.error('Error retrieving cards:', error);
-    res.status(500).json({ error: 'Failed to retrieve cards' });
-  }
-});
+  // Get the credentials from Redis
+  redis.get('credentials', (err, storedCredentials) => {
+    if (err) {
+      console.error('Error retrieving credentials from Redis:', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
 
-// Create a new card
-app.post('/cards', async (req, res) => {
-  try {
-    // await redis.flushdb();
+    // Parse the stored credentials from JSON
+    const credentials = JSON.parse(storedCredentials);
 
+    // Check if the username exists in the credentials
+    if (credentials.hasOwnProperty(username) && password === credentials[username]) {
+      // Passwords match, return successful login status
+      return res.json({ status: 'success', message: 'Login successful' });
+    }
 
-    const { id, sides } = req.body;
-    const card = { id, sides };
-    await redis.hset(`card:${id}`, card);
-    await redis.rpush('cards', id);
-    res.status(201).json(card);
-  } catch (error) {
-    console.error('Error creating card:', error);
-    res.status(500).json({ error: 'Failed to create card' });
-  }
-});
-
-// Update an existing card
-app.put('/cards/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { sides, rate, reviewedAt } = req.body;
-    const card = { id, sides, rate, reviewedAt };
-    await redis.hset(`card:${id}`, card);
-    res.json(card);
-  } catch (error) {
-    console.error('Error updating card:', error);
-    res.status(500).json({ error: 'Failed to update card' });
-  }
+    // Invalid username or password    
+    return res.json({ status: 'error', message: 'Invalid username or password' });
+  });
 });
 
 app.listen(3000, () => {
